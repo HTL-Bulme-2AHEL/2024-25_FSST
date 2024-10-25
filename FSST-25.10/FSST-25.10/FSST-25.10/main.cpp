39d295fcc2a04e452f99830d00ebac731c41523b
Christoph Schwaighofer <christoph@hwaildesign.com>
2024-10-25 08:49:27 +0200

    Stunde 25.10

--- FSST-25.10/FSST-25.10/FSST-25.10/main.cpp
+++ FSST-25.10/FSST-25.10/FSST-25.10/main.cpp
@@ -5,8 +5,14 @@ using namespace std;
 
 #define MAX_WERTE 50
 
-char wurf[20];
+char wurf[MAX_WERTE];
 int zufallszahl;
+
+int kopf = 0;
+int zahl = 0;
+int folge = 0;
+
+bool bool_zahl = false;
 
 int main() {
 	srand(time(0));
@@ -16,11 +22,21 @@ int main() {
 
 		if (zufallszahl == 1) {
 			wurf[i] = 'Z';
+			zahl++;
+			bool_zahl = true;
 		}
 		else {
 			wurf[i] = 'K';
+			kopf++;
+			
+			if (bool_zahl == true) {
+				folge++;
+			}
+			bool_zahl = false;
 		}
 		cout << wurf[i] << endl;
 	}
+
+	cout << endl << endl << "Es kam Zahl " << zahl << " Mal vor und Kopf " << kopf << " Mal vor." << endl << "Es kam Z gefolgt von K " << folge << " Mal vor.";
 	return 0;
 }
